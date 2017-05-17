c3bdedb1606638946d57342541dc5ed5094e2969
Ashley Trinh <atrnh@teknik.io>
2017-05-17 10:45:50 -0700

    Game chooses a random word on reload.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import Hangmin from './Hangmin';
 import './index.css';
 
-const secret_word = 'hangmin';
+const WORDS = [
+  'hangmin',
+  'indubitably',
+  'hopefully',
+  'octopuses',
+  'arbitrary',
+  'disgusting',
+  'adoration',
+];
+const secret_word = WORDS[Math.floor(Math.random() * WORDS.length)];
 
 ReactDOM.render(
   <Hangmin word={secret_word} />,
